285975339e57e2190ade492fe919d5dff2170bdd
Antiavk <avk_tp@mail.ru>
2023-03-04 22:33:43 +0300

    Initial commit

---
+++ src/main/java/springbootconditional/profile/ProductionProfile.java
@@ -0,0 +1,8 @@
+package springbootconditional.profile;
+
+public class ProductionProfile implements SystemProfile{
+    @Override
+    public String getProfile() {
+        return "Current profile is production";
+    }
+}
---
+++ src/main/java/springbootconditional/profile/SystemProfile.java
@@ -0,0 +1,5 @@
+package springbootconditional.profile;
+
+public interface SystemProfile {
+    String getProfile();
+}
