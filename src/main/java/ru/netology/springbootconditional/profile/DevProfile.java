c567cc82aef08fbb7c4d363134f355b425397be0
Antiavk <avk_tp@mail.ru>
2023-03-12 17:41:57 +0300

    Testcontainers

--- src/main/java/ru/netology/springbootconditional/profile/DevProfile.java
+++ src/main/java/ru/netology/springbootconditional/profile/DevProfile.java
@@ -3,6 +3,7 @@ package ru.netology.springbootconditional.profile;
 public class DevProfile implements SystemProfile {
     @Override
     public String getProfile() {
+
         return "Current profile is dev";
     }
 }
--- src/main/java/ru/netology/springbootconditional/profile/ProductionProfile.java
+++ src/main/java/ru/netology/springbootconditional/profile/ProductionProfile.java
@@ -3,6 +3,7 @@ package ru.netology.springbootconditional.profile;
 public class ProductionProfile implements SystemProfile{
     @Override
     public String getProfile() {
+
         return "Current profile is production";
     }
 }
